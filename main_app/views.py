6e1230bf7d1a8b35d9d79f44819b64b33eea2edb
Marco Arteaga <marcoarteaga26@gmail.com>
2019-12-14 05:09:37 -0600

    new about page created

--- main_app/views.py
+++ main_app/views.py
@@ -4,3 +4,6 @@ from django.http import HttpResponse
 
 def home(request):
     return HttpResponse('<h1>Homepage for Finches</h1>')
+
+def about(request):
+    return HttpResponse('<h1>About page for Finches</h1>')
