4f53b3035bcef52ce5e1c9336c883a1ec961822c
Marco Arteaga <marcoarteaga26@gmail.com>
2019-12-14 06:37:44 -0600

    finch class is created and data is migrated

--- main_app/urls.py
+++ main_app/urls.py
@@ -4,5 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('about/', views.about, name='about'),
-    path('finches/' views.finches_index, name='index')
+    path('finches/', views.finches_index, name='index')
 ]
